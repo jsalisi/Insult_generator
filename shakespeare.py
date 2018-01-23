8b8d862835a6524c9405e56eff6a54d51c9a05b1
emilieez <emilie.m.zhang@gmail.com>
2018-01-23 15:31:27 -0800

    Finish GUI

--- shakespeare.py
+++ shakespeare.py
@@ -1,5 +1,7 @@
 import random
 
+from guizero import App, Text, PushButton
+
 list_a = []
 list_b = []
 list_c = []
@@ -9,14 +11,22 @@ def insult_me():
     word_a = random.choice(list_a)
     word_b = random.choice(list_b)
     word_c = random.choice(list_c)
-    print("Thou", word_a, word_b, word_c, '!')
+    return '{} {} {} {} {}'.format("Thou", word_a, word_b, word_c, '!')
 
 
-with open("insults.csv", "r") as f:
-    for line in f:
-        lineArr = line.rstrip().split(',')
-        list_a.append(lineArr[0])
-        list_b.append(lineArr[1])
-        list_c.append(lineArr[2])
+def new_insult():
+    message.set(insult_me())
 
-insult_me()
+
+if __name__ == '__main__':
+    with open("insults.csv", "r") as f:
+        for line in f:
+            lineArr = line.rstrip().split(',')
+            list_a.append(lineArr[0])
+            list_b.append(lineArr[1])
+            list_c.append(lineArr[2])
+
+    app = App("Shakespearean insult generator")
+    message = Text(app, insult_me())
+    button = PushButton(app, new_insult, text="Insult me again")
+    app.display()
