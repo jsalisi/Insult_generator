39d50f91aa04ab67564a32d9e1f0ef55340809fc
jsalisi <jsalisi@gmail.com>
2018-01-29 23:16:47 -0800

    minor format edits

--- shakespeare.py
+++ shakespeare.py
@@ -1,10 +1,22 @@
-import random
+# import random
 
-from guizero import App, Text, PushButton, Combo
+from guizero import App, Text, Combo  # , PushButton
 
 list_a = []
 list_b = []
 list_c = []
+
+'''
+def insult_me():
+    word_a = random.choice(list_a)
+    word_b = random.choice(list_b)
+    word_c = random.choice(list_c)
+    return '{} {} {} {} {}'.format("Thou", word_a, word_b, word_c, '!')
+
+
+def new_insult():
+    message.set(insult_me())
+'''
 
 def insults_menu(app_name, insult_list):
     position = 2
@@ -20,12 +32,15 @@ if __name__ == '__main__':
             list_b.append(lineArr[1])
             list_c.append(lineArr[2])
 
-    app = App(title = "Shakespearean insult generator", layout = "grid")
+    app = App(title="Shakespearean insult generator", layout="grid")
 
-    thou = Text(app, "Thou", grid=[1,0], align="left")
+    thou = Text(app, "Thou", grid=[1, 0], align="left")
 
     insults_menu(app, [list_a, list_b, list_c])
 
-    exclamation = Text(app, "!", grid=[5,0], align="left")
+    exclamation = Text(app, "!", grid=[5, 0], align="left")
+
+    # message = Text(app, insult_me())
+    # button = PushButton(app, new_insult, text="Insult me again")
 
     app.display()
