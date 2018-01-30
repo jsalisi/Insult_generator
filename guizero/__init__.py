fda79ef3a04d1d777ab3df5f6cc3aa8a281c8ec2
jsalisi <jsalisi@gmail.com>
2018-01-29 23:06:42 -0800

    Changed UI to pull down menus, added files to guizero folder

--- guizero/__init__.py
+++ guizero/__init__.py
@@ -9,16 +9,16 @@ from . import utilities as utils
 from .alerts import *
 
 from .App import App
-from .Box import Box
-from .ButtonGroup import ButtonGroup
-from .CheckBox import CheckBox
+# from .Box import Box
+# from .ButtonGroup import ButtonGroup
+# from .CheckBox import CheckBox
 from .Combo import Combo
-from .MenuBar import MenuBar
-from .Picture import Picture
+# from .MenuBar import MenuBar
+# from .Picture import Picture
 from .PushButton import PushButton
-from .RadioButton import RadioButton
-from .Slider import Slider
+# from .RadioButton import RadioButton
+# from .Slider import Slider
 from .Text import Text
-from .TextBox import TextBox
+# from .TextBox import TextBox
 from .PushButton import PushButton
-from .Waffle import Waffle
+# from .Waffle import Waffle
--- shakespeare.py
+++ shakespeare.py
@@ -1,22 +1,16 @@
 import random
 
-from guizero import App, Text, PushButton
+from guizero import App, Text, PushButton, Combo
 
 list_a = []
 list_b = []
 list_c = []
 
-
-def insult_me():
-    word_a = random.choice(list_a)
-    word_b = random.choice(list_b)
-    word_c = random.choice(list_c)
-    return '{} {} {} {} {}'.format("Thou", word_a, word_b, word_c, '!')
-
-
-def new_insult():
-    message.set(insult_me())
-
+def insults_menu(app_name, insult_list):
+    position = 2
+    for insults in insult_list:
+        Combo(app_name, options=insults, grid=[position, 0], align="left")
+        position += 1
 
 if __name__ == '__main__':
     with open("insults.csv", "r") as f:
@@ -26,7 +20,12 @@ if __name__ == '__main__':
             list_b.append(lineArr[1])
             list_c.append(lineArr[2])
 
-    app = App("Shakespearean insult generator")
-    message = Text(app, insult_me())
-    button = PushButton(app, new_insult, text="Insult me again")
+    app = App(title = "Shakespearean insult generator", layout = "grid")
+
+    thou = Text(app, "Thou", grid=[1,0], align="left")
+
+    insults_menu(app, [list_a, list_b, list_c])
+
+    exclamation = Text(app, "!", grid=[5,0], align="left")
+
     app.display()
