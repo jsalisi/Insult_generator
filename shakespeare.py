db3f7d58a07ab5d83ab237a718b07dc0db5a0179
jsalisi <jsalisi@gmail.com>
2018-01-29 23:51:31 -0800

    changed functionality

--- shakespeare.py
+++ shakespeare.py
@@ -1,28 +1,25 @@
-# import random
+import random
 
-from guizero import App, Text, Combo  # , PushButton
+from guizero import App, Text, Combo, PushButton
 
 list_a = []
 list_b = []
 list_c = []
 
-'''
+
 def insult_me():
     word_a = random.choice(list_a)
     word_b = random.choice(list_b)
     word_c = random.choice(list_c)
-    return '{} {} {} {} {}'.format("Thou", word_a, word_b, word_c, '!')
+    return '{} {} {} {}{}'.format("Thou", word_a, word_b, word_c, '!')
 
 
 def new_insult():
     message.set(insult_me())
-'''
 
-def insults_menu(app_name, insult_list):
-    position = 2
-    for insults in insult_list:
-        Combo(app_name, options=insults, grid=[position, 0], align="left")
-        position += 1
+def set_insult():
+    message.set('{} {} {} {}{}'.format("Thou", menu1.value, menu2.value, menu3.value, '!'))
+
 
 if __name__ == '__main__':
     with open("insults.csv", "r") as f:
@@ -34,13 +31,13 @@ if __name__ == '__main__':
 
     app = App(title="Shakespearean insult generator", layout="grid")
 
-    thou = Text(app, "Thou", grid=[1, 0], align="left")
+    message = Text(app, insult_me(), grid=[0, 0], align="left")
+    rand_button = PushButton(app, new_insult, text="Insult me again", pady=5, padx=5, grid=[1, 0], align="left")
 
-    insults_menu(app, [list_a, list_b, list_c])
+    menu1 = Combo(app, options=list_a, grid=[0, 2], align="top")
+    menu2 = Combo(app, options=list_b, grid=[0, 3], align="top")
+    menu3 = Combo(app, options=list_c, grid=[0, 4], align="top")
 
-    exclamation = Text(app, "!", grid=[5, 0], align="left")
-
-    # message = Text(app, insult_me())
-    # button = PushButton(app, new_insult, text="Insult me again")
+    use_button = PushButton(app, set_insult, text="Use insult", pady=5, padx=5, grid=[0, 5], align="top")
 
     app.display()
