55ed01775aab904a22826063acabdad9f687ce55
Raspberry Pi <jsalisi@gmail.com>
2018-01-30 11:37:20 +0000

    adjusted window size

--- shakespeare.py
+++ shakespeare.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
             list_b.append(lineArr[1])
             list_c.append(lineArr[2])
 
-    app = App(title="Shakespearean insult generator", layout="grid")
+    app = App(title="Shakespearean insult generator", width=450, height=300, layout="grid")
 
     message = Text(app, insult_me(), grid=[0, 0], align="left")
     rand_button = PushButton(app, new_insult, text="Insult me again", pady=5, padx=5, grid=[1, 0], align="left")
